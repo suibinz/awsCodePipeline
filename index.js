d8dd469fcc55e1a7b51d653d6fbaa2046840bf85
suibinz <suibinzh@gmail.com>
2018-08-22 11:06:58 -0700

    add weather info

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var time = require('time');
+var weather = require('weather-js');
+
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
+    var currentTime = new time.Date();
+	var currentWeather;
     currentTime.setTimezone("America/Los_Angeles");
+	weaterh.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result){
+			if (err) console.log(err);
+			currentWeather = result
+	});
+
     callback(null, {
         statusCode: 200,
-        body: 'The time in Redwood City is: ' + currentTime.toString(),
+        body: 'The time in Redwood City is: ' + currentTime.toString() + "\n" + 'The weather in Redwood City is:' + JSON.stringify(currentWeather, null, 2),
     });
 };
