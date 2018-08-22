4d9b916ad79c724f1066ee4ccbc4ff069b945d05
suibinz <suibinzh@gmail.com>
2018-08-21 22:25:06 -0700

    fix return code as int

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
-        statusCode: '200',
+        statusCode: 200,
         body: 'The time in Redwood City is: ' + currentTime.toString(),
     });
 };
