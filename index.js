a458f6cd743b189900ffd85e51aa1ec2b2e67367
suibinz <suibinzh@gmail.com>
2018-08-22 14:13:06 -0700

    fix wether

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ var weather = require('weather-js');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
 	var currentWeather = [];
+	var weatherStr;
     currentTime.setTimezone("America/Los_Angeles");
 	weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result){
-			if (err) console.log(err);
-			currentWeather = result;
-			//console.log(JSON.stringify(result, null, 2));
+		if (err) console.log(err);
+    	
+		callback(null, {
+        	statusCode: 200,
+        	body: 'The time in Redwood City is: ' + currentTime.toString() + "\n" + 'The weather in Redwood City is: ' + JSON.stringify(result, null, 2) });
 	});
-			//console.log(JSON.stringify(currentWeather, null, 2));
-	var weatherStr = JSON.stringify(currentWeather[0], null, 2);
-	console.log("Weather String: " + weatherStr);
-	console.log(JSON.stringify(currentWeather, null, 2));
-    
-	callback(null, {
-        statusCode: 200,
-        body: 'The time in Redwood City is: ' + currentTime.toString() + "\n" + 'The weather in Redwood City is: ' + JSON.stringify(currentWeather, null, 2)
-    });
 };
