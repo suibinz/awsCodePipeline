b3a4e4f8118785d25a3d39d614a737a9ebbb7f28
suibinz <suibinzh@gmail.com>
2018-08-22 13:12:33 -0700

    TS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var weather = require('weather-js');
 
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-	var currentWeather;
+	var currentWeather = [];
     currentTime.setTimezone("America/Los_Angeles");
 	weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result){
 			if (err) console.log(err);
@@ -11,7 +11,7 @@ exports.handler = (event, context, callback) => {
 			//console.log(JSON.stringify(result, null, 2));
 	});
 			//console.log(JSON.stringify(currentWeather, null, 2));
-			var weatherStr = JSON.stringify(currentWeather, null, 2);
+			var weatherStr = JSON.stringify(currentWeather[0], null, 2);
 			console.log("Weather String: " + weatherStr)
     callback(null, {
         statusCode: 200,
